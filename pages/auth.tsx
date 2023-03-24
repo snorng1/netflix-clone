d940475a629cc9ae39175952a1064aad6b66a46b
snorng1 <89033297+snorng1@users.noreply.github.com>
2023-03-24 14:32:08 -0400

    Update

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Input from "@/components/Input";
 
+
 const Auth = () => {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpeg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -13,8 +18,36 @@ const Auth = () => {
                             Sign in
                         </h2>
                         <div className="flex flex-col gap-4">
-                            <Input></Input>
+                            <Input 
+                                label="Username"
+                                onChange={(ev: any) => setName(ev.target.value)}
+                                id="name"
+                                value={name}
+                            />
+                            <Input 
+                                label="Email"
+                                onChange={(ev: any) => setEmail(ev.target.value)}
+                                id="email"
+                                type="email"
+                                value={email}
+                            />
+                            <Input 
+                                label="Password"
+                                onChange={(ev: any) => setPassword(ev.target.value)}
+                                id="password"
+                                type="password"
+                                value={password}
+                            />
                         </div>
+                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            Login
+                        </button>
+                        <p className="text-neutral-500 mt-12">
+                            First time using Netflix?
+                            <span className="text-white ml-1 hover:underline cursor-pointer">
+                                Create and account
+                            </span>
+                        </p>
                     </div>
                 </div>
             </div>
